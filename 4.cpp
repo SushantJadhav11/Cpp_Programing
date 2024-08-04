6e1a4f8da0b1da059b2d089d60d5d1d9e308461d
SushantJadhav11 <sushantja232@gmail.com>
2024-08-04 14:02:35 +0530

    C++ programs

---
+++ 4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int slices = 5;
+    cout << "you have " << slices << " slices of pizza" << endl;
+
+    return 0;
+}
---
+++ 6.cpp
@@ -0,0 +1,19 @@
+// comments are no read by the complier.
+/*hi everyone
+this the way
+to wtite comments that are multiline.*/
+
+// functions
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int base, exponent;
+    cout << "What is the base?";
+    cin >> base;
+    cout << "what is the exponent?";
+    cin >> exponent;
+    cout << pow(base, exponent);
+}

d59393a364777387bd803e37eaef9fe988007ce7
SushantJadhav11 <114638164+SushantJadhav11@users.noreply.github.com>
2024-08-26 20:56:36 +0530

    Add files via upload

---
+++ voidfunction.cpp
@@ -0,0 +1,13 @@
+// cpp is case sensitive
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int base, component;
+    cout << "Enter the base";
+    cin >> base;
+    cout << "ENter the power componemt";
+    cin >> component;
+    cout << pow(base, component);
+}

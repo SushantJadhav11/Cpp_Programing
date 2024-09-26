a90b3495ac9418bcc5d75de21f22c7fc3ef8885b
SushantJadhav11 <114638164+SushantJadhav11@users.noreply.github.com>
2024-09-26 18:41:17 +0530

    Add files via upload

--- loop.cpp
+++ loop.cpp
@@ -11,6 +11,10 @@ int main()
         cout << "You're not old enough\n";
         return -1; // it will not run always statement
     }
+    else
+    {
+        return 0;
+    }
     cout << "always";
     return 0;
 }

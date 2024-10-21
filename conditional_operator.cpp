5ac7a3502ecbe3e064598d4f2822d20fffb7c269
SushantJadhav11 <114638164+SushantJadhav11@users.noreply.github.com>
2024-10-21 12:54:46 +0530

    Add files via upload

--- conditional_operator.cpp
+++ conditional_operator.cpp
@@ -4,4 +4,11 @@ using namespace std;
 int main()
 {
     int answer = 11;
+    cout << "guess the number:" << endl;
+    int guess;
+    cin >> guess;
+    int points = guess == answer ? 10 : 0;
+    guess == answer ? cout << "good job! " : cout << "bad job!";
+
+    cout << "You have " << points << " points." << endl;
 }
